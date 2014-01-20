2d2ab9ee299a85d8bbd1c6c287137fcbc6920d77
Taylor Savage <taylor@taylorsavage.com>
2014-01-20 15:17:20 -0800

    got rid of Optimist

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-var argv = require('optimist')
-			.usage('Usage: $0 [config file]')
-			.argv;
-
-
 var CanvasClient = require('./client');
-console.log(argv);
 
-var client = new CanvasClient((require(argv._[0])));
+var client = new CanvasClient((require(process.argv[2])));
 client.connect();
