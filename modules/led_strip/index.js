01ed75b88bcd6a96e107da52926b40928985b7a3
Taylor Savage <taylor@taylorsavage.com>
2014-01-20 16:19:50 -0800

    basic lights on off

--- modules/led_strip/index.js
+++ modules/led_strip/index.js
@@ -1,10 +1,14 @@
 var util = require('util');
 var canvasModule = require('../canvas-module');
+var spi = require('spi');
 
 function LedStrip(options) {
 	canvasModule.BaseModule.call(this);
 
 	this.options = options;
+
+	var spiDevice = new spi.Spi('/dev/spidev0.0', {});
+	this.lights = new LightStrips('/dev/spidev0.0', options.leds, spiDevice);
 
 	this.on("turnOn", this.turnOn.bind(this));
 	this.on("turnOff", this.turnOff.bind(this));
@@ -13,11 +17,14 @@ util.inherits(LedStrip, canvasModule.BaseModule);
 
 LedStrip.prototype.turnOn = function() {
 	console.log('led turn on');
+	this.lights.all(255, 255, 255);
+	this.lights.sync();
 	this.emit("event", "turnedOn");
 };
 
 LedStrip.prototype.turnOff = function() {
 	console.log('led turn off');
+	this.lights.off();
 	this.emit("event", "turnedOff");
 };
 
